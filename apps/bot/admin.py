4c968a922740effc97b1b4f08fb9c357cbf81bf7
Dilshodbek Donaboyev <d.donaboyev@uic.group>
2025-09-08 17:00:41 +0500

    fix: sms pending admin

--- apps/bot/admin.py
+++ apps/bot/admin.py
@@ -8,3 +8,8 @@ admin.site.register(SmsPenndingBot)
 class TelegramUserAdmin(admin.ModelAdmin):
     list_display = ("id", "first_name", "last_name", "username", "telegram_id")
     list_display_links = ("id", "first_name")
+
+@admin.register(SmsPenndingBot)
+class SMSPenndingAdmin(admin.ModelAdmin):
+    list_display = ("id", "phone", "uuid")
+    list_display_links = ("id", "phone")
