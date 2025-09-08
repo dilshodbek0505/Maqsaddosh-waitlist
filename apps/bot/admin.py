f6ffce18f24525b100825525b00f0951953e99a8
Dilshodbek Donaboyev <d.donaboyev@uic.group>
2025-09-08 17:03:46 +0500

    fix: sms pennding admin

--- apps/bot/admin.py
+++ apps/bot/admin.py
@@ -2,7 +2,6 @@ from django.contrib import admin
 
 from apps.bot.models import SmsPenndingBot, TelegramUser
 
-admin.site.register(SmsPenndingBot)
 
 @admin.register(TelegramUser)
 class TelegramUserAdmin(admin.ModelAdmin):
